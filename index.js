26b7247f8994faaa4c5fa9b95672aa6ab3e35dee
Leah Silber <LeahSilber@gmail.com>
2020-03-07 14:37:22 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.className = 'victory'
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = 'Leah is the champion'
+
+
+let nixMe = document.querySelector('main#main')
+nixMe.remove()
